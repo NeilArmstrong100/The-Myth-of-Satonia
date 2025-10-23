b382a4b56f1f18a68dc50341d36a272a42565a0c
Chase Gordon <123526091+NeilArmstrong100@users.noreply.github.com>
2025-10-23 12:43:24 -0400

    Delete src directory

--- src/main/font.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <fstream>
-#include <vector>
-#include <GLFW/glfw3.h>
-#include "../stb/stb_truetype.h"
-
-void load_font(const char*);
-void draw_text(float, float, const char*, float);
