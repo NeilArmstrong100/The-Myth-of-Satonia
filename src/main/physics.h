4ad05e0ee0982be513b5d874ed30fe18921ad95f
Chase Gordon <123526091+NeilArmstrong100@users.noreply.github.com>
2025-12-15 23:15:42 -0500

    Delete src directory

--- src/main/physics.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../entity/component.h"
-#include "../entity/registry.h"
-#include "../tile/tile.h"
-
-void run();
-void physics_update(const float);
-void start(Registry&);
-void event_handler(float);
